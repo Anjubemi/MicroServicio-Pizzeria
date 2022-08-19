ad739f8570eb4299743a0d44bc09ad5b6cd42577
Anjubemi <antony77_af@hotmail.com>
2022-08-18 23:34:01 -0500

    Proyecto Inicial

---
+++ src/main/java/com/idat/MicroservicioPizzeria/EfAntonyBecerraMicroservicioPizzeriaApplication.java
@@ -0,0 +1,13 @@
+package com.idat.MicroservicioPizzeria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EfAntonyBecerraMicroservicioPizzeriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EfAntonyBecerraMicroservicioPizzeriaApplication.class, args);
+	}
+
+}
